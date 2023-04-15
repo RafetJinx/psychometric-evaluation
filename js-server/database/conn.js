1b190506be23e381f4e96c208103411f3af9663e
RafetJinx <rafet.ersoy.cs@gmail.com>
2023-04-15 19:51:43 +0300

    connected to mongodb

---
+++ js-server/database/conn.js
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
+
+export default async function connect(){
+    await mongoose.connect(process.env.ATLAS_URI);
+    console.log("Database connected");
+}
--- js-server/server.js
+++ js-server/server.js
@@ -5,13 +5,18 @@ import cors from 'cors';
 import { config } from 'dotenv'
 import router from './router/route.js';
 
+
+/** import connection file */
+import connect from './database/conn.js';
+config();
+
+
 const app = express();
 
 /** app middlewares */
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-config();
 
 /** application port */
 const port = process.env.PORT || 8080;
@@ -28,6 +33,17 @@ app.get('/', (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server connected to http://localhost:${port}`);
-});
+
+/**start server only when we have a valid connection*/
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        });
+    } catch(error){
+        console.log("Cannot connect to the server");
+    }
+}).catch(error => {
+    console.log("Invalid Database connection")
+})
+
