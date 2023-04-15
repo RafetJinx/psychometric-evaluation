3e4ac4faf811c20c949a86fe7a15db013e33d335
RafetJinx <rafet.ersoy.cs@gmail.com>
2023-04-15 16:55:24 +0300

    created result & question controllers

--- js-server/server.js
+++ js-server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import morgan from 'morgan';
 import cors from 'cors';
 import { config } from 'dotenv'
+import router from './router/route.js';
 
 const app = express();
 
@@ -17,6 +18,8 @@ const port = process.env.PORT || 8080;
 
 
 /** routes */
+app.use('/api', router); /** api's */
+
 app.get('/', (req, res) => {
     try {
         res.json("Get Request")
