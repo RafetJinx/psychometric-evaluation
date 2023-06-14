e956e0a4e6e913e6e1b7fd091515dbeb198bb6eb
RafetJinx <rafet.ersoy.cs@gmail.com>
2023-06-15 01:01:11 +0300

    added Admin, Test, User controller files

---
+++ java-server/src/main/java/server/example/javaServer/JavaServerApplication.java
@@ -0,0 +1,29 @@
+package server.example.javaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@SpringBootApplication
+@EnableSwagger2
+public class JavaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+}
---
+++ java-server/src/test/java/server/example/javaServer/JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package server.example.javaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
